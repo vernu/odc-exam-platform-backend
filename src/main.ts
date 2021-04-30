792440fc441cc6d4616fec8a1dab2188961eb81e
isra el <vernu1997@gmail.com>
2021-04-30 15:25:53 +0300

    added alternative PORT in main.ts if PORT is not set in environment variable

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
