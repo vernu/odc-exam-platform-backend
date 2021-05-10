1d5511bab972c732a3c47d462813bd89cc0f3544
isra el <vernu1997@gmail.com>
2021-05-10 15:02:59 +0300

    added title and description to exam schema

--- src/exams/schemas/exam.schema.ts
+++ src/exams/schemas/exam.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-import { Organization } from 'src/organizations/schemas/organization.schema';
+import { Organization } from '../../organizations/schemas/organization.schema';
 import { User } from '../../users/schemas/user.schema';
 import { Question } from './question.schema';
 
@@ -8,6 +8,10 @@ export type ExamDocument = Exam & Document;
 
 @Schema({ timestamps: true })
 export class Exam {
+  @Prop({ type: String, required: true })
+  title: string;
+  @Prop({ type: String })
+  description: string;
   @Prop({ type: Types.ObjectId, ref: Organization.name })
   organization: Organization;
   @Prop({ type: [Types.ObjectId] })
