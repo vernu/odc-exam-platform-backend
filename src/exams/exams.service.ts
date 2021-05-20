cf62af944d269c267098d4511b4c9cbc9520f587
isra el <vernu1997@gmail.com>
2021-05-20 20:18:52 +0300

    created a route for getting list of questions for a specific exam

--- src/exams/exams.service.ts
+++ src/exams/exams.service.ts
@@ -102,7 +102,22 @@ export class ExamsService {
     }
   }
 
-  async getExamQuestions(examId: string) {}
+  async getExamQuestions(examId: string) {
+    const exam = await this.examModel.findById(examId);
+    if (!exam) {
+      throw new HttpException(
+        {
+          success: false,
+          error: 'Exam not found',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const questions = await this.questionModel.find({
+      exam: exam._id,
+    });
+    return questions;
+  }
 
   async deleteExam(examId: string) {
     const exam = await this.examModel.findById(examId);
