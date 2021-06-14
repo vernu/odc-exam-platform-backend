6af915cdc871f9cc1ffe50adb1173fc57dfc1e83
isra el <vernu1997@gmail.com>
2021-06-14 20:42:24 +0300

    access control for update examinee-answer (points Gained) route

--- src/exams/examinee-answers/examinee-answers.controller.ts
+++ src/exams/examinee-answers/examinee-answers.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Param, Patch, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { UpdateExamineeAnswerDTO } from '../dto/examinee-answer.dto';
+import { CanUpdateExamineeAnswer } from '../guards/can-update-examinee-answer';
 import { ExamineeAnswersService } from './examinee-answers.service';
 
 @Controller('examinee-answers')
 export class ExamineeAnswersController {
   constructor(private examineeAnswersService: ExamineeAnswersService) {}
   @Patch(':examineeAnswerId')
+  @UseGuards(JwtAuthGuard, CanUpdateExamineeAnswer)
   async updateExamineeAnswer(
     @Param('examineeAnswerId') examineeAnswerId: string,
     @Body() updateExamineeAnswerDTO: UpdateExamineeAnswerDTO,
