114e81b9773c740fe9669c50b98533fe0d80bf60
isra el <vernu1997@gmail.com>
2021-06-16 15:39:39 +0300

    created update an individual question route

---
+++ src/exams/questions/questions.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Param, Patch } from '@nestjs/common';
+import { UpdateQuestionDTO } from '../dto/question.dto';
+import { QuestionsService } from './questions.service';
+
+@Controller('questions')
+export class QuestionsController {
+  constructor(private questionsService: QuestionsService) {}
+  @Patch(':questionId')
+  async updateQuestion(
+    @Param('questionId') questionId: string,
+    @Body() updateQuestionDTO: UpdateQuestionDTO,
+  ) {
+    await this.questionsService.updateQuestion(questionId, updateQuestionDTO);
+    return { success: true, message: 'question has been updated' };
+  }
+}
---
+++ src/exams/questions/questions.service.ts
@@ -0,0 +1,43 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UpdateQuestionDTO } from '../dto/question.dto';
+import { QuestionDocument } from '../schemas/question.schema';
+
+@Injectable()
+export class QuestionsService {
+  constructor(
+    @InjectModel('Question')
+    private questionModel: Model<QuestionDocument>,
+  ) {}
+
+  async updateQuestion(
+    questionId: string,
+    updateQuestionDTO: UpdateQuestionDTO,
+  ) {
+    const question = await this.questionModel.findOne({
+      _id: questionId,
+    });
+    if (!question) {
+      throw new HttpException(
+        {
+          success: false,
+          error: 'question not found',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    try {
+      await question.updateOne(updateQuestionDTO);
+    } catch (e) {
+      throw new HttpException(
+        {
+          success: false,
+          error: `coudnt update exam question : ${e.toString()}`,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
