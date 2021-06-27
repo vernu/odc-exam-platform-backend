c0d9e72eb56e6e617d59d9cb5f0f73befb04b37e
isra el <vernu1997@gmail.com>
2021-06-27 21:01:38 +0300

    added mean, median, mode to exam stats

--- src/exams/exams.service.ts
+++ src/exams/exams.service.ts
@@ -799,6 +799,9 @@ export class ExamsService {
       allScores,
       averageScore,
       scoresRange,
+      mean: this.calculateMean(allScores),
+      median: this.calculateMedian(allScores),
+      mode: this.calculateMode(allScores),
       lowestScores,
       highestScores,
       fastestResponses,
@@ -810,5 +813,57 @@ export class ExamsService {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  calculateMean(numbers: number[]) {
+    var total = 0,
+      i;
+    for (i = 0; i < numbers.length; i += 1) {
+      total += numbers[i];
+    }
+    return total / numbers.length;
+  }
+  calculateMedian(numbers: number[]) {
+    var median = 0,
+      numsLen = numbers.length;
+    numbers.sort();
+
+    if (
+      numsLen % 2 ===
+      0 // is even
+    ) {
+      // average of two middle numbers
+      median = (numbers[numsLen / 2 - 1] + numbers[numsLen / 2]) / 2;
+    } else {
+      // is odd
+      // middle number only
+      median = numbers[(numsLen - 1) / 2];
+    }
+
+    return median;
+  }
+  calculateMode(numbers: number[]) {
+    var modes = [],
+      count = [],
+      i,
+      number,
+      maxIndex = 0;
+
+    for (i = 0; i < numbers.length; i += 1) {
+      number = numbers[i];
+      count[number] = (count[number] || 0) + 1;
+      if (count[number] > maxIndex) {
+        maxIndex = count[number];
+      }
+    }
+
+    for (i in count)
+      if (count.hasOwnProperty(i)) {
+        if (count[i] === maxIndex) {
+          modes.push(Number(i));
+        }
+      }
+
+    return modes;
   }
 }
